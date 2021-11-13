8828eab5c2525774f3e2836ae12b1c7cae47ac67
Md Obaydul Hoque <84621516+odht82@users.noreply.github.com>
2021-11-13 19:04:11 +0600

    updated footer and subscription section

--- src/Components/Pages/Footer/Footer.js
+++ src/Components/Pages/Footer/Footer.js
@@ -17,8 +17,7 @@ function Footer() {
                     Subscribe to Get Special Price
                 </h2>
                 <p className='footer-subscription-text'>
-                    Don’t wanna miss something? <br /> Subscribe right now and get special promotion and <br />
-                    monthly Newsletters
+                    Don’t wanna miss something? <br /> Subscribe right now and get special promotions
                 </p>
                 <div className='input-areas'>
                     <form>
@@ -28,50 +27,18 @@ function Footer() {
                             type='email'
                             placeholder='Your Email'
                         />
+                        <button className='subscribe-button'>Subscribe</button>
                     </form>
                 </div>
             </section>
-            <div className='footer-links'>
-                <div className='footer-link-wrapper'>
-                    <div className='footer-link-items'>
-                        <h2>About Us</h2>
-                        <Link to='/sign-up'>How it works</Link>
-                        <Link to='/'>Careers</Link>
-                        <Link to='/'>Investors</Link>
-                        <Link to='/'>Terms of Service</Link>
-                    </div>
-                    <div className='footer-link-items'>
-                        <h2>Contact Us</h2>
-                        <Link to='/'>Contact</Link>
-                        <Link to='/'>Support</Link>
-                        <Link to='/'>Customer care</Link>
-                        <Link to='/'>Sponsorships</Link>
-                    </div>
-                </div>
-                <div className='footer-link-wrapper'>
-                    <div className='footer-link-items'>
-                        <h2>Videos</h2>
-                        <Link exact to='/products'>Bookings</Link>
-                        <Link exact to='/doctors'>Packages</Link>
-                        <Link to='/'>Influencer</Link>
-                    </div>
-                    <div className='footer-link-items'>
-                        <h2>Social Media</h2>
-                        <Link to='/'>Instagram</Link>
-                        <Link to='/'>Facebook</Link>
-                        <Link to='/'>Youtube</Link>
-                        <Link to='/'>Twitter</Link>
-                    </div>
-                </div>
-            </div>
             <section className='social-media'>
                 <div className='social-media-wrap'>
                     <div className='footer-logo'>
                         <Link to='/' className='social-logo'>
-                            <h3>Hoque Travel.</h3>
+                            <h3>Fragrance-Shop.</h3>
                         </Link>
                     </div>
-                    <small className='website-rights'>Hoque Travel Allright Reserved | 2021</small>
+                    <small className='website-rights'>Fragrance-Shop Allright Reserved | 2021</small>
                     <div className='social-icons'>
                         <Link
                             className='social-icon-link'
